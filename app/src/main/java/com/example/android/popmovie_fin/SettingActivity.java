5476d8ab6baace4099f1e51dd5a1e67bb5cad25d
linwei-yoh <395373035@qq.com>
2016-09-18 16:45:55 +0800

    5th commit

--- app/src/main/java/com/example/android/popmovie_fin/SettingActivity.java
+++ app/src/main/java/com/example/android/popmovie_fin/SettingActivity.java
@@ -5,15 +5,17 @@ import android.annotation.TargetApi;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
+import android.support.v4.app.NavUtils;
+import android.view.MenuItem;
 
 
-public class SettingActivity extends PreferenceActivity {
+public class SettingActivity extends AppCompatPreferenceActivity  {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.setting_pref);
+
     }
 
     //在双屏模式下时候 从Setting界面回退时 detailfragment会是空的
@@ -22,5 +24,17 @@ public class SettingActivity extends PreferenceActivity {
     @Override
     public Intent getParentActivityIntent() {
         return super.getParentActivityIntent().addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+    }
+
+    @Override
+    public boolean onMenuItemSelected(int featureId, MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            if (!super.onMenuItemSelected(featureId, item)) {
+                NavUtils.navigateUpFromSameTask(this);
+            }
+            return true;
+        }
+        return super.onMenuItemSelected(featureId, item);
     }
 }
